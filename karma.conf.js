f5a42af1b1d976352cabd1ef766ae9b297c696f0
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-07-18 11:35:24 +0300

    added node_modules to gitignore, identation

--- karma.conf.js
+++ karma.conf.js
@@ -55,7 +55,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['Chrome', 'PhantomJS'];
 
 
 // If browser does not capture in given timeout [ms], kill it
