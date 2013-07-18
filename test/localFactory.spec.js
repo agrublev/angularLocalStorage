c52dc6acad01df89692f12f1cd3fd41904c580ac
Thomas Lohmüller <thomas@lohmueller.ch>
2013-07-18 18:13:48 +0200

    tests

--- test/localFactory.spec.js
+++ test/localFactory.spec.js
@@ -71,6 +71,53 @@ describe('angular-localStorage module', function () {
 		});
 	});
 
+	describe('when bind() $scope field to localStorage with different storeName', function () {
+		beforeEach(function () {
+			inject(function ($rootScope) {
+				scope = $rootScope.$new();
+
+				scope.$apply(function () {
+					scope.spec = true;
+				});
+
+				$store.bind(scope, 'spec', 'defaultValue', 'specStore');
+
+				scope.$apply(function () {
+					scope.spec = false;
+				});
+			});
+		});
+
+		beforeEach(function () {
+			testValue = $store.get('specStore');
+		});
+
+		it('should have $scope value', function () {
+			expect(testValue).toEqual(scope.spec);
+		});
+	});
+
+	describe('when unbind() variable that clears localStorage and the variable when using a different storeName', function () {
+		var testLocalStorageValue, testLocalVariableValue;
+
+		beforeEach(function () {
+			$store.unbind(scope, 'spec', 'specStore');
+		});
+
+		beforeEach(function () {
+			testLocalStorageValue = $store.get('spec');
+			testLocalVariableValue = scope.spec;
+		});
+
+		it('should not contain field in storage', function () {
+			expect(testLocalStorageValue).toBeNull();
+		});
+
+		it('should not contain field in scope', function () {
+			expect(testLocalVariableValue).toBeNull();
+		});
+	});
+
 	describe('when remove() field from localStorage', function () {
 		beforeEach(function () {
 			$store.remove('spec');
