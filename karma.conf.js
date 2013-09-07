be350d7126653de6d210ed3916f37fed83e98bdf
agrublev <agrublev@gmail.com>
2013-09-07 12:46:42 -0700

    more changes to make karma work

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
+// Karma configuration
+
 module.exports = function ( config ) {
 	config.set({
-		// Karma configuration
-
 		// base path, that will be used to resolve files and exclude
 		basePath : '',
 
@@ -12,36 +12,28 @@ module.exports = function ( config ) {
 			'components/angular/angular.js', 'components/angular-cookies/angular-cookies.js', 'components/angular-mocks/angular-mocks.js', 'src/*.js', 'test/*.spec.js'
 		],
 
-
 		// list of files to exclude
 		exclude : [],
-
 
 		// test results reporter to use
 		// possible values: 'dots', 'progress', 'junit'
 		reporters : ['progress'],
 
-
 		// web server port
 		port : 9876,
-
 
 		// cli runner port
 		runnerPort : 9100,
 
-
 		// enable / disable colors in the output (reporters and logs)
 		colors : true,
-
 
 		// level of logging
 		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
 		logLevel : 'karma.LOG_INFO',
 
-
 		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch : true,
-
 
 		// Start these browsers, currently available:
 		// - Chrome
@@ -53,10 +45,8 @@ module.exports = function ( config ) {
 		// - IE (only Windows)
 		browsers : ['Chrome', 'PhantomJS'],
 
-
 		// If browser does not capture in given timeout [ms], kill it
 		captureTimeout : 60000,
-
 
 		// Continuous Integration mode
 		// if true, it capture browsers, run tests and exit
